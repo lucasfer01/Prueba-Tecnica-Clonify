756c004538517d61f6b253181ebb8c49ccb870ec
lucasfer01 <lucasfer9912@gmail.com>
2022-01-28 01:57:04 -0300

    Conexion a base de datos

--- Backend/index.js
+++ Backend/index.js
@@ -10,13 +10,21 @@ const { PORT } = process.env;
 const { rutas } = require('./src/routes/index.routes');
 // Morgan
 const morgan = require('morgan');
+// Sequelize
+const { sequelize } = require('./src/database/db');
 
 // Middleware
-app.use(express.json()); 
+app.use(express.json());
 app.use(morgan('dev'));
 app.use('/', rutas);
 
 // Ponemos el servidor a la escucha de peticiones
 app.listen(PORT, () => {
+    // Mensaje de exito
     console.log(`Escuchando en puerto ${PORT}`);
+
+    // Conectar a base de datos
+    sequelize.sync({ force: true })
+             .then(() => console.log(`Conectado correctamente a DB ${process.env.POSTGRES_DBNAME}`)) // Si la conexion es exitosa
+             .catch(error => console.log({ error: error })); // Si ocurre algun error
 });
