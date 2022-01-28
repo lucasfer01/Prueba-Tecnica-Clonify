1e462ae55fb4a553ffed0c8fd5b7f44b97706b90
lucasfer01 <lucasfer9912@gmail.com>
2022-01-28 01:23:51 -0300

    Incorporamos morgan

--- Backend/index.js
+++ Backend/index.js
@@ -8,8 +8,12 @@ require('dotenv').config();
 const { PORT } = process.env;
 // Rutas
 const { rutas } = require('./src/routes/index.routes');
+// Morgan
+const morgan = require('morgan');
 
 // Middleware
+app.use(express.json()); 
+app.use(morgan('dev'));
 app.use('/', rutas);
 
 // Ponemos el servidor a la escucha de peticiones
