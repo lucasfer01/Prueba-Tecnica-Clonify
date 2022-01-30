b967e3066eb2edeff1ec5919645c1c1186e2842a
lucasfer01 <lucasfer9912@gmail.com>
2022-01-30 06:18:23 -0300

    Boton de agregar post completado, formulario de agregar post completa, funcionalidad de app completada

--- Frontend/src/Componentes/Rutas/PrivateRoutes/PrivateRoutes.jsx
+++ Frontend/src/Componentes/Rutas/PrivateRoutes/PrivateRoutes.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 // React-router-dom
 import { Routes, Route } from 'react-router-dom';
 // Componentes
+import { AgregarPost } from '../../AgregarPost/AgregarPost';
 import { EditarPost } from '../../EditarPost/EditarPost';
 import { Home } from '../../Home/Home';
 
@@ -11,8 +12,9 @@ export function PrivateRoutes() {
         <div>
             {/* Estas rutas se van a renderizar si el usuario esat autenticado */}
             <Routes>
-                <Route path="home" element={<Home />}/>
-                <Route path="editarPost/:postId" element={<EditarPost/>}/>
+                <Route path="home" element={<Home />} />
+                <Route path="editarPost/:postId" element={<EditarPost />} />
+                <Route path="crearPost" element={<AgregarPost />} />
             </Routes>
         </div>
     );
