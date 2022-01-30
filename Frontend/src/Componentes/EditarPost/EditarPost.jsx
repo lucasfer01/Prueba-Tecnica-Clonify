e2677bfb8079aa6ac7e7a9cc7530ddd280ec2ff3
lucasfer01 <lucasfer9912@gmail.com>
2022-01-30 03:50:55 -0300

    formulario de edicion de post completado

--- Frontend/src/Componentes/EditarPost/EditarPost.jsx
+++ Frontend/src/Componentes/EditarPost/EditarPost.jsx
@@ -12,6 +12,8 @@ import estilosEditarPost from './EditarPost.module.css';
 import spinner from '../../media/spinner.jpg';
 // Cloudinary controller
 import { subirImagenes } from '../../Cloudinary/Cloudinary.controller';
+// React-icons
+import { AiFillDelete } from 'react-icons/ai';
 
 export function EditarPost() {
     // Obtenemos el id del post con useParams
@@ -31,7 +33,9 @@ export function EditarPost() {
     const [inputsErrors, setInputsErrors] = useState({
         titulo: false,
         descripcion: false
-    })
+    });
+    // Estado que permite enviar el formulario
+    const [canSendForm, setCanSendForm] = useState(true);
 
     // UseEffect
     useEffect(() => {
@@ -56,31 +60,43 @@ export function EditarPost() {
 
             // Verificamos que no este vacio
             if (e.target.value.length === 0) {
-                // Seteamos el estado de errorde titulo en true
+                // Seteamos el estado de error de titulo en true
                 setInputsErrors({
                     ...inputsErrors, // Copiamos el estado
                     titulo: true // Seteamos que hay un error
-                })
+                });
+
+                // Seteamos que el formulario no puede ser enviado
+                setCanSendForm(false);
             } else { // Si esta todo correcto
                 // Cambiamos el esatdo solo si esta en true
                 inputsErrors.titulo && setInputsErrors({
                     ...inputsErrors, // 
                     titulo: false
-                })
+                });
+
+                // Si titulo y descripcion son false entonces no tienene errores, por ende podemos enviar el formulario
+                (!inputsErrors.titulo && !inputsErrors.descripcion) && setCanSendForm(true); // Si ambos son false seteamos el estado de canSendForm en true
             }
         } else { // Si no es el input titulo es de de descripcion
-            if(e.target.value.length === 0) { // Si el valor del campo descripcion es 0
+            if (e.target.value.length === 0) { // Si el valor del campo descripcion es 0
                 // Seteamos el estado de error de descripcion en true
                 setInputsErrors({
                     ...inputsErrors, // Copiamos el estado
                     descripcion: true // Hay un error, seteamos el estado en true
-                })
+                });
+
+                // Seteamos que el formulario no puede ser enviado
+                setCanSendForm(false);
             } else { // Si esta todo correcto
                 // Cambiamos el estado solo si esta en true
                 inputsErrors.descripcion && setInputsErrors({
                     ...inputsErrors, // Copiamos el estado
                     descripcion: false
-                })
+                });
+
+                // Si titulo y descripcion son false entonces no tienene errores, por ende podemos enviar el formulario
+                (!inputsErrors.titulo && !inputsErrors.descripcion) && setCanSendForm(true); // Si ambos son false seteamos el estado de canSendForm en true
             }
         }
 
@@ -118,7 +134,7 @@ export function EditarPost() {
         e.preventDefault();
 
         // Enviar modificaciones
-        axios.put(`${POST_BACK_URL}/${postId}`, post)
+        canSendForm && axios.put(`${POST_BACK_URL}/${postId}`, post)
             .then(response => navigate('/home')) // Redirigimos al home
             .catch(error => console.log(error));
     }
@@ -130,46 +146,53 @@ export function EditarPost() {
         </div>
     ) : (
         <div className={estilosEditarPost.contenedor}>
-            <form onSubmit={handleOnSubmit}>
-                <div>
-                    <label>Titulo</label>
-                    <input type="text" value={post.post_title} name='post_title' onChange={handleOnChange} />
+            <form className={estilosEditarPost.formulario} onSubmit={handleOnSubmit}>
+                <div className={estilosEditarPost.divFormulario}>
+                    <label className={estilosEditarPost.label}>Titulo</label>
+                    <input className={estilosEditarPost.tituloInput} type="text" value={post.post_title} name='post_title' onChange={handleOnChange} />
                     {inputsErrors.titulo && (
-                        <ul>
+                        <ul className={estilosEditarPost.error}>
                             <li>El campo no puede estar vacio</li>
                             <li>No puede tener mas de 100 caracteres</li>
                         </ul>
                     )}
                 </div>
-                <div>
-                    <label>Descripcion</label>
-                    <textarea name="post_description" value={post.post_description} onChange={handleOnChange}></textarea>
+                <div className={estilosEditarPost.divFormulario}>
+                    <label className={estilosEditarPost.label}>Descripcion</label>
+                    <textarea className={estilosEditarPost.descripcionInput} name="post_description" value={post.post_description} onChange={handleOnChange}></textarea>
                     {inputsErrors.descripcion && (
-                        <ul>
+                        <ul className={estilosEditarPost.error}>
                             <li>El campo no puede estar vacio</li>
                         </ul>
                     )}
                 </div>
-                <div>
-                    <label>Fotos</label>
+                <div className={estilosEditarPost.divFormulario}>
+                    <label className={estilosEditarPost.label}>Fotos</label>
                     <div>
                         {post.post_image.length ? ( // Si hay imagenes mapeamos el array y renderizamos
-                            <div>
-                                <img src={post.post_image} width='200px' alt='Foto post' />
-                                <button onClick={() => setPost({
+                            <div className={estilosEditarPost.contenedorImagen}>
+                                <img src={post.post_image} width='400px' alt='Foto post' />
+                                <button className={estilosEditarPost.eliminarFotoBoton} onClick={() => setPost({
                                     ...post,
                                     post_image: []
                                 })}
-                                >x</button>
+                                >
+                                    <AiFillDelete fill='#fff' />
+                                </button>
                             </div>
-                        ) : ''}
+                        ) : (
+                            <div className={estilosEditarPost.contenedorImagen}>
+                                <span>No hay <br /> imagen cargada</span>
+                            </div>
+                        )}
 
                     </div>
-                    <input type="file" onChange={handleAddFoto} />
+                    <input className={estilosEditarPost.inputFile} type="file" onChange={handleAddFoto} />
                     {cargandoImagen && <h3>Cargando imagen...</h3>}
                 </div>
 
-                <button type='submit'>Actualizar post</button>
+                <button className={estilosEditarPost.enviarFormularioBoton} disabled={!canSendForm} type='submit'>Actualizar post</button>
+                {!canSendForm && <h4>No se puede actualizar el post debido <br /> a un error en alguno de los campos</h4>}
             </form>
         </div>
     )
